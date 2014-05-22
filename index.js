afc8885ba6dc7de29766891fdf217c300f5d882f
Alex <alex@mybuilding.org>
2014-05-22 13:34:55 -0400

    index

---
+++ index.js
@@ -0,0 +1,6 @@
+var fs     = require('fs');
+var xml2js = require('xml2js');
+var ig     = require('imagemagick');
+var colors = require('colors');
+var _      = require('underscore');
+var Q      = require('q');
