e94f59046938abcfb71f45a9556eb8ccbbd07c15
Chenjia <ariesjia00@hotmail.com>
2014-12-31 13:55:17 +0800

    Update index.js
    add support for android xxxhdpi icon

--- index.js
+++ index.js
@@ -48,6 +48,7 @@ var getPlatforms = function (projectName) {
             { name : 'drawable-mdpi/icon.png',  size : 48 },
             { name : 'drawable-xhdpi/icon.png', size : 96 },
             { name : 'drawable-xxhdpi/icon.png', size : 144 },
+            { name : 'drawable-xxxhdpi/icon.png', size : 192 },
         ]
     });
     // TODO: add all platforms
