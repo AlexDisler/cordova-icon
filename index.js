6506b515a8048f8c6634063ba01756326b9b2447
Gunar C. Gessner <gunar@gunargessner.com>
2015-10-01 21:29:41 -0300

    Ensure specified image proportions

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ var generateIcon = function (platform, icon) {
         quality: 1,
         format: 'png',
         width: icon.size,
-        height: icon.size,
+        height: icon.size+'!',
     } , function(err, stdout, stderr){
         if (err) {
             deferred.reject(err);
