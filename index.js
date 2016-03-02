3230d148f6bdc4d1b18b39390f43a315f98afb93
Samuel Santos <ssantos@present-technologies.com>
2016-03-02 20:00:03 +0000

    Add missing settings icon
    For iPhone 6 Plus.

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ var getPlatforms = function (projectName) {
             { name : 'icon-76@2x.png',    size : 152 },
             { name : 'icon-small.png',    size : 29  },
             { name : 'icon-small@2x.png', size : 58  },
+            { name : 'icon-small@3x.png', size : 87  },
             { name : 'icon.png',          size : 57  },
             { name : 'icon@2x.png',       size : 114 },
             { name : 'icon-83.5@2x.png',  size : 167 }
