ff959f90a4945d0ba02ab00a255af006e2966c06
Alex Disler <alexdisler@gmail.com>
2016-03-26 14:32:11 -0400

    spaces 4 -> 2

--- index.js
+++ index.js
@@ -14,92 +14,92 @@ var wrench = require('wrench');
  * @return {Promise} resolves with an array of platforms
  */
 var getPlatforms = function (projectName) {
-    var deferred = Q.defer();
-    var platforms = [];
-    platforms.push({
-        name : 'ios',
-        // TODO: use async fs.exists
-        isAdded : fs.existsSync('platforms/ios'),
-        iconsPath : 'platforms/ios/' + projectName + '/Images.xcassets/AppIcon.appiconset/',
-        icons : [
-            { name : 'icon-40.png',       size : 40  },
-            { name : 'icon-40@2x.png',    size : 80  },
-            { name : 'icon-50.png',       size : 50  },
-            { name : 'icon-50@2x.png',    size : 100 },
-            { name : 'icon-60.png',       size : 60  },
-            { name : 'icon-60@2x.png',    size : 120 },
-            { name : 'icon-60@3x.png',    size : 180 },
-            { name : 'icon-72.png',       size : 72  },
-            { name : 'icon-72@2x.png',    size : 144 },
-            { name : 'icon-76.png',       size : 76  },
-            { name : 'icon-76@2x.png',    size : 152 },
-            { name : 'icon-small.png',    size : 29  },
-            { name : 'icon-small@2x.png', size : 58  },
-            { name : 'icon-small@3x.png', size : 87  },
-            { name : 'icon.png',          size : 57  },
-            { name : 'icon@2x.png',       size : 114 },
-            { name : 'icon-83.5@2x.png',  size : 167 }
-        ]
-    });
-    platforms.push({
-        name : 'android',
-        isAdded : fs.existsSync('platforms/android'),
-        iconsPath : 'platforms/android/res/',
-        icons : [
-            { name : 'drawable/icon.png',       size : 96 },
-            { name : 'drawable-hdpi/icon.png',  size : 72 },
-            { name : 'drawable-ldpi/icon.png',  size : 36 },
-            { name : 'drawable-mdpi/icon.png',  size : 48 },
-            { name : 'drawable-xhdpi/icon.png', size : 96 },
-            { name : 'drawable-xxhdpi/icon.png', size : 144 },
-            { name : 'drawable-xxxhdpi/icon.png', size : 192 }
-        ]
-    });
-    platforms.push({
-        name : 'windows',
-        isAdded : fs.existsSync('platforms/windows'),
-        iconsPath : 'platforms/windows/images/',
-        icons : [
-            { name : 'StoreLogo.scale-100.png', size : 50  },
-            { name : 'StoreLogo.scale-125.png', size : 63  },
-            { name : 'StoreLogo.scale-150.png', size : 75  },
-            { name : 'StoreLogo.scale-200.png', size : 100 },
-            { name : 'StoreLogo.scale-400.png', size : 200 },
+  var deferred = Q.defer();
+  var platforms = [];
+  platforms.push({
+    name : 'ios',
+    // TODO: use async fs.exists
+    isAdded : fs.existsSync('platforms/ios'),
+    iconsPath : 'platforms/ios/' + projectName + '/Images.xcassets/AppIcon.appiconset/',
+    icons : [
+      { name : 'icon-40.png',       size : 40  },
+      { name : 'icon-40@2x.png',    size : 80  },
+      { name : 'icon-50.png',       size : 50  },
+      { name : 'icon-50@2x.png',    size : 100 },
+      { name : 'icon-60.png',       size : 60  },
+      { name : 'icon-60@2x.png',    size : 120 },
+      { name : 'icon-60@3x.png',    size : 180 },
+      { name : 'icon-72.png',       size : 72  },
+      { name : 'icon-72@2x.png',    size : 144 },
+      { name : 'icon-76.png',       size : 76  },
+      { name : 'icon-76@2x.png',    size : 152 },
+      { name : 'icon-small.png',    size : 29  },
+      { name : 'icon-small@2x.png', size : 58  },
+      { name : 'icon-small@3x.png', size : 87  },
+      { name : 'icon.png',          size : 57  },
+      { name : 'icon@2x.png',       size : 114 },
+      { name : 'icon-83.5@2x.png',  size : 167 }
+    ]
+  });
+  platforms.push({
+    name : 'android',
+    isAdded : fs.existsSync('platforms/android'),
+    iconsPath : 'platforms/android/res/',
+    icons : [
+      { name : 'drawable/icon.png',       size : 96 },
+      { name : 'drawable-hdpi/icon.png',  size : 72 },
+      { name : 'drawable-ldpi/icon.png',  size : 36 },
+      { name : 'drawable-mdpi/icon.png',  size : 48 },
+      { name : 'drawable-xhdpi/icon.png', size : 96 },
+      { name : 'drawable-xxhdpi/icon.png', size : 144 },
+      { name : 'drawable-xxxhdpi/icon.png', size : 192 }
+    ]
+  });
+  platforms.push({
+    name : 'windows',
+    isAdded : fs.existsSync('platforms/windows'),
+    iconsPath : 'platforms/windows/images/',
+    icons : [
+      { name : 'StoreLogo.scale-100.png', size : 50  },
+      { name : 'StoreLogo.scale-125.png', size : 63  },
+      { name : 'StoreLogo.scale-150.png', size : 75  },
+      { name : 'StoreLogo.scale-200.png', size : 100 },
+      { name : 'StoreLogo.scale-400.png', size : 200 },
 
-            { name : 'Square44x44Logo.scale-100.png', size : 44  },
-            { name : 'Square44x44Logo.scale-125.png', size : 55  },
-            { name : 'Square44x44Logo.scale-150.png', size : 66  },
-            { name : 'Square44x44Logo.scale-200.png', size : 88  },
-            { name : 'Square44x44Logo.scale-400.png', size : 176 },
+      { name : 'Square44x44Logo.scale-100.png', size : 44  },
+      { name : 'Square44x44Logo.scale-125.png', size : 55  },
+      { name : 'Square44x44Logo.scale-150.png', size : 66  },
+      { name : 'Square44x44Logo.scale-200.png', size : 88  },
+      { name : 'Square44x44Logo.scale-400.png', size : 176 },
 
-            { name : 'Square71x71Logo.scale-100.png', size : 71  },
-            { name : 'Square71x71Logo.scale-125.png', size : 89  },
-            { name : 'Square71x71Logo.scale-150.png', size : 107 },
-            { name : 'Square71x71Logo.scale-200.png', size : 142 },
-            { name : 'Square71x71Logo.scale-400.png', size : 284 },
+      { name : 'Square71x71Logo.scale-100.png', size : 71  },
+      { name : 'Square71x71Logo.scale-125.png', size : 89  },
+      { name : 'Square71x71Logo.scale-150.png', size : 107 },
+      { name : 'Square71x71Logo.scale-200.png', size : 142 },
+      { name : 'Square71x71Logo.scale-400.png', size : 284 },
 
-            { name : 'Square150x150Logo.scale-100.png', size : 150 },
-            { name : 'Square150x150Logo.scale-125.png', size : 188 },
-            { name : 'Square150x150Logo.scale-150.png', size : 225 },
-            { name : 'Square150x150Logo.scale-200.png', size : 300 },
-            { name : 'Square150x150Logo.scale-400.png', size : 600 },
+      { name : 'Square150x150Logo.scale-100.png', size : 150 },
+      { name : 'Square150x150Logo.scale-125.png', size : 188 },
+      { name : 'Square150x150Logo.scale-150.png', size : 225 },
+      { name : 'Square150x150Logo.scale-200.png', size : 300 },
+      { name : 'Square150x150Logo.scale-400.png', size : 600 },
 
-            { name : 'Square310x310Logo.scale-100.png', size : 310  },
-            { name : 'Square310x310Logo.scale-125.png', size : 388  },
-            { name : 'Square310x310Logo.scale-150.png', size : 465  },
-            { name : 'Square310x310Logo.scale-200.png', size : 620  },
-            { name : 'Square310x310Logo.scale-400.png', size : 1240 },
+      { name : 'Square310x310Logo.scale-100.png', size : 310  },
+      { name : 'Square310x310Logo.scale-125.png', size : 388  },
+      { name : 'Square310x310Logo.scale-150.png', size : 465  },
+      { name : 'Square310x310Logo.scale-200.png', size : 620  },
+      { name : 'Square310x310Logo.scale-400.png', size : 1240 },
 
-            { name : 'Wide310x150Logo.scale-100.png', size : 310, height : 150  },
-            { name : 'Wide310x150Logo.scale-125.png', size : 388, height : 188  },
-            { name : 'Wide310x150Logo.scale-150.png', size : 465, height : 225  },
-            { name : 'Wide310x150Logo.scale-200.png', size : 620, height : 300  },
-            { name : 'Wide310x150Logo.scale-400.png', size : 1240, height : 600 }
-        ]
-    });
-    // TODO: add missing platforms
-    deferred.resolve(platforms);
-    return deferred.promise;
+      { name : 'Wide310x150Logo.scale-100.png', size : 310, height : 150  },
+      { name : 'Wide310x150Logo.scale-125.png', size : 388, height : 188  },
+      { name : 'Wide310x150Logo.scale-150.png', size : 465, height : 225  },
+      { name : 'Wide310x150Logo.scale-200.png', size : 620, height : 300  },
+      { name : 'Wide310x150Logo.scale-400.png', size : 1240, height : 600 }
+    ]
+  });
+  // TODO: add missing platforms
+  deferred.resolve(platforms);
+  return deferred.promise;
 };
 
 
@@ -116,17 +116,17 @@ settings.ICON_FILE   = 'icon.png';
  */
 var display = {};
 display.success = function (str) {
-    str = '✓  '.green + str;
-    console.log('  ' + str);
+  str = '✓  '.green + str;
+  console.log('  ' + str);
 };
 display.error = function (str) {
-    str = '✗  '.red + str;
-    console.log('  ' + str);
+  str = '✗  '.red + str;
+  console.log('  ' + str);
 };
 display.header = function (str) {
-    console.log('');
-    console.log(' ' + str.cyan.underline);
-    console.log('');
+  console.log('');
+  console.log(' ' + str.cyan.underline);
+  console.log('');
 };
 
 /**
@@ -135,21 +135,21 @@ display.header = function (str) {
  * @return {Promise} resolves to a string - the project's name
  */
 var getProjectName = function () {
-    var deferred = Q.defer();
-    var parser = new xml2js.Parser();
-    data = fs.readFile(settings.CONFIG_FILE, function (err, data) {
-        if (err) {
-            deferred.reject(err);
-        }
-        parser.parseString(data, function (err, result) {
-            if (err) {
-                deferred.reject(err);
-            }
-            var projectName = result.widget.name[0];
-            deferred.resolve(projectName);
-        });
+  var deferred = Q.defer();
+  var parser = new xml2js.Parser();
+  data = fs.readFile(settings.CONFIG_FILE, function (err, data) {
+    if (err) {
+      deferred.reject(err);
+    }
+    parser.parseString(data, function (err, result) {
+      if (err) {
+        deferred.reject(err);
+      }
+      var projectName = result.widget.name[0];
+      deferred.resolve(projectName);
     });
-    return deferred.promise;
+  });
+  return deferred.promise;
 };
 
 /**
@@ -160,50 +160,50 @@ var getProjectName = function () {
  * @return {Promise}
  */
 var generateIcon = function (platform, icon) {
-    var deferred = Q.defer();
-    var srcPath = settings.ICON_FILE;
-    var platformPath = srcPath.replace(/\.png$/, '-' + platform.name + '.png');
-    if (fs.existsSync(platformPath)) {
-        srcPath = platformPath;
+  var deferred = Q.defer();
+  var srcPath = settings.ICON_FILE;
+  var platformPath = srcPath.replace(/\.png$/, '-' + platform.name + '.png');
+  if (fs.existsSync(platformPath)) {
+    srcPath = platformPath;
+  }
+  var dstPath = platform.iconsPath + icon.name;
+  var dst = path.dirname(dstPath);
+  if (!fs.existsSync(dst)) {
+    wrench.mkdirSyncRecursive(dst);
+  }
+  ig.resize({
+    srcPath: srcPath,
+    dstPath: dstPath,
+    quality: 1,
+    format: 'png',
+    width: icon.size,
+    height: icon.size
+  } , function(err, stdout, stderr){
+    if (err) {
+      deferred.reject(err);
+    } else {
+      deferred.resolve();
+      display.success(icon.name + ' created');
     }
-    var dstPath = platform.iconsPath + icon.name;
-    var dst = path.dirname(dstPath);
-    if (!fs.existsSync(dst)) {
-        wrench.mkdirSyncRecursive(dst);
-    }
-    ig.resize({
-        srcPath: srcPath,
-        dstPath: dstPath,
-        quality: 1,
-        format: 'png',
-        width: icon.size,
-        height: icon.size
+  });
+  if (icon.height) {
+    ig.crop({
+      srcPath: srcPath,
+      dstPath: dstPath,
+      quality: 1,
+      format: 'png',
+      width: icon.size,
+      height: icon.height
     } , function(err, stdout, stderr){
-        if (err) {
-            deferred.reject(err);
-        } else {
-            deferred.resolve();
-            display.success(icon.name + ' created');
-        }
+      if (err) {
+        deferred.reject(err);
+      } else {
+        deferred.resolve();
+        display.success(icon.name + ' cropped');
+      }
     });
-    if (icon.height) {
-      ig.crop({
-          srcPath: srcPath,
-          dstPath: dstPath,
-          quality: 1,
-          format: 'png',
-          width: icon.size,
-          height: icon.height
-      } , function(err, stdout, stderr){
-          if (err) {
-              deferred.reject(err);
-          } else {
-              deferred.resolve();
-              display.success(icon.name + ' cropped');
-          }
-      });
-    }
-    return deferred.promise;
+  }
+  return deferred.promise;
 };
 
 /**
@@ -213,19 +213,19 @@ var generateIcon = function (platform, icon) {
  * @return {Promise}
  */
 var generateIconsForPlatform = function (platform) {
-    var deferred = Q.defer();
-    display.header('Generating Icons for ' + platform.name);
-    var all = [];
-    var icons = platform.icons;
-    icons.forEach(function (icon) {
-        all.push(generateIcon(platform, icon));
-    });
-    Q.all(all).then(function () {
-        deferred.resolve();
-    }).catch(function (err) {
-        console.log(err);
-    });
-    return deferred.promise;
+  var deferred = Q.defer();
+  display.header('Generating Icons for ' + platform.name);
+  var all = [];
+  var icons = platform.icons;
+  icons.forEach(function (icon) {
+    all.push(generateIcon(platform, icon));
+  });
+  Q.all(all).then(function () {
+    deferred.resolve();
+  }).catch(function (err) {
+    console.log(err);
+  });
+  return deferred.promise;
 };
 
 /**
@@ -235,19 +235,19 @@ var generateIconsForPlatform = function (platform) {
  * @return {Promise}
  */
 var generateIcons = function (platforms) {
-    var deferred = Q.defer();
-    var sequence = Q();
-    var all = [];
-    _(platforms).where({ isAdded : true }).forEach(function (platform) {
-        sequence = sequence.then(function () {
-            return generateIconsForPlatform(platform);
-        });
-        all.push(sequence);
+  var deferred = Q.defer();
+  var sequence = Q();
+  var all = [];
+  _(platforms).where({ isAdded : true }).forEach(function (platform) {
+    sequence = sequence.then(function () {
+      return generateIconsForPlatform(platform);
     });
-    Q.all(all).then(function () {
-        deferred.resolve();
-    });
-    return deferred.promise;
+    all.push(sequence);
+  });
+  Q.all(all).then(function () {
+    deferred.resolve();
+  });
+  return deferred.promise;
 };
 
 /**
@@ -256,20 +256,20 @@ var generateIcons = function (platforms) {
  * @return {Promise} resolves if at least one platform was found, rejects otherwise
  */
 var atLeastOnePlatformFound = function () {
-    var deferred = Q.defer();
-    getPlatforms().then(function (platforms) {
-        var activePlatforms = _(platforms).where({ isAdded : true });
-        if (activePlatforms.length > 0) {
-            display.success('platforms found: ' + _(activePlatforms).pluck('name').join(', '));
-            deferred.resolve();
-        } else {
-            display.error('No cordova platforms found.' +
-                'Make sure you are in the root folder of your Cordova project' +
-                'and add platforms with \'cordova platform add\'');
-            deferred.reject();
-        }
-    });
-    return deferred.promise;
+  var deferred = Q.defer();
+  getPlatforms().then(function (platforms) {
+    var activePlatforms = _(platforms).where({ isAdded : true });
+    if (activePlatforms.length > 0) {
+      display.success('platforms found: ' + _(activePlatforms).pluck('name').join(', '));
+      deferred.resolve();
+    } else {
+      display.error('No cordova platforms found.' +
+                    'Make sure you are in the root folder of your Cordova project' +
+                      'and add platforms with \'cordova platform add\'');
+                      deferred.reject();
+    }
+  });
+  return deferred.promise;
 };
 
 /**
@@ -278,17 +278,17 @@ var atLeastOnePlatformFound = function () {
  * @return {Promise} resolves if exists, rejects otherwise
  */
 var validIconExists = function () {
-    var deferred = Q.defer();
-    fs.exists(settings.ICON_FILE, function (exists) {
-        if (exists) {
-            display.success(settings.ICON_FILE + ' exists');
-            deferred.resolve();
-        } else {
-            display.error(settings.ICON_FILE + ' does not exist in the root folder');
-            deferred.reject();
-        }
-    });
-    return deferred.promise;
+  var deferred = Q.defer();
+  fs.exists(settings.ICON_FILE, function (exists) {
+    if (exists) {
+      display.success(settings.ICON_FILE + ' exists');
+      deferred.resolve();
+    } else {
+      display.error(settings.ICON_FILE + ' does not exist in the root folder');
+      deferred.reject();
+    }
+  });
+  return deferred.promise;
 };
 
 /**
@@ -297,31 +297,31 @@ var validIconExists = function () {
  * @return {Promise} resolves if exists, rejects otherwise
  */
 var configFileExists = function () {
-    var deferred = Q.defer();
-    fs.exists(settings.CONFIG_FILE, function (exists) {
-        if (exists) {
-            display.success(settings.CONFIG_FILE + ' exists');
-            deferred.resolve();
-        } else {
-            display.error('cordova\'s ' + settings.CONFIG_FILE + ' does not exist in the root folder');
-            deferred.reject();
-        }
-    });
-    return deferred.promise;
+  var deferred = Q.defer();
+  fs.exists(settings.CONFIG_FILE, function (exists) {
+    if (exists) {
+      display.success(settings.CONFIG_FILE + ' exists');
+      deferred.resolve();
+    } else {
+      display.error('cordova\'s ' + settings.CONFIG_FILE + ' does not exist in the root folder');
+      deferred.reject();
+    }
+  });
+  return deferred.promise;
 };
 
 display.header('Checking Project & Icon');
 
 atLeastOnePlatformFound()
-    .then(validIconExists)
-    .then(configFileExists)
-    .then(getProjectName)
-    .then(getPlatforms)
-    .then(generateIcons)
-    .catch(function (err) {
-        if (err) {
-            console.log(err);
-        }
-    }).then(function () {
-        console.log('');
-    });
+  .then(validIconExists)
+  .then(configFileExists)
+  .then(getProjectName)
+  .then(getPlatforms)
+  .then(generateIcons)
+  .catch(function (err) {
+    if (err) {
+      console.log(err);
+    }
+  }).then(function () {
+    console.log('');
+  });
