1d05a472a50db64435739e560b1a5370933f4b57
Zarko Hristovski <zarko@timegrip.no>
2019-03-27 11:11:59 +0100

    Match new icons/filenames from cordova-ios 5.0.0
    Refer to https://github.com/apache/cordova-ios/pull/512

--- index.js
+++ index.js
@@ -51,19 +51,18 @@ var getPlatforms = function (projectName) {
       { name: 'icon-76@2x.png',          size : 152  },
       { name: 'icon-83.5@2x.png',        size : 167  },
       { name: 'icon-1024.png',           size : 1024 },
-      { name: 'icon-small.png',          size : 29   },
-      { name: 'icon-small@2x.png',       size : 58   },
-      { name: 'icon-small@3x.png',       size : 87   },
       { name: 'icon.png',                size : 57   },
       { name: 'icon@2x.png',             size : 114  },
-      { name: 'AppIcon24x24@2x.png',     size : 48   },
-      { name: 'AppIcon27.5x27.5@2x.png', size : 55   },
-      { name: 'AppIcon29x29@2x.png',     size : 58   },
-      { name: 'AppIcon29x29@3x.png',     size : 87   },
-      { name: 'AppIcon40x40@2x.png',     size : 80   },
-      { name: 'AppIcon44x44@2x.png',     size : 88   },
-      { name: 'AppIcon86x86@2x.png',     size : 172  },
-      { name: 'AppIcon98x98@2x.png',     size : 196  }
+      // Match new icons/filenames from cordova-ios 5.0.0
+      // See https://github.com/apache/cordova-ios/pull/512
+      { name: 'icon-24@2x.png',          size : 48   },
+      { name: 'icon-27.5@2x.png',        size : 55   },
+      { name: 'icon-29.png',             size : 29   },
+      { name: 'icon-29@2x.png',          size : 58   },
+      { name: 'icon-29@3x.png',          size : 87   },
+      { name: 'icon-44@2x.png',          size : 88   },
+      { name: 'icon-86@2x.png',          size : 172  },
+      { name: 'icon-98@2x.png',          size : 196  }
     ]
   });
   platforms.push({
